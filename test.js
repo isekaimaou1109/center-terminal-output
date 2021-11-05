f58518fe3eaf9b131079b0af23fe13057e0da4f7
isekaimaou1109 <nhat6739@gmail.com>
2021-11-05 16:57:43 +0700

    file này để test thôi

---
+++ test.js
@@ -0,0 +1,26 @@
+var { align } = require('center-terminal-output')
+
+console.log(align('Hello world', 'left')) // đoạn text này dc căn lề trái
+console.log(align('Hello world', 'left', 12)) // đoạn text này dc căn lề trái với 12 lần nhấn space
+console.log(align('Hello world', 'left', 36)) // đoạn text này dc căn lề trái với 36 lần nhấn space
+
+console.log('\n')
+console.log("=".repeat(60))
+console.log('\n')
+
+console.log(align('Hello world', 'right')) // đoạn text này dc căn lề phải
+console.log(align('Hello world', 'right', 12)) // đoạn text này dc căn lề phải với 12 lần nhấn space
+console.log(align('Hello world', 'right', 36)) // đoạn text này dc căn lề phải với 36 lần nhấn space
+
+console.log('\n')
+console.log("=".repeat(60))
+console.log('\n')
+
+console.log(align('Text in justified mode', 'center')) // đoạn text này dc căn chính giữa
+
+console.log('\n')
+console.log("=".repeat(60))
+console.log('\n')
+
+// đoạn text này sẽ bị lỗi vì kết hợp kiểu center và indent lại sẽ cause error
+console.log(align('Hello world', 'center', 1)) 
